d5c5803115bbada1b64c4940c9f6fd0b3771d470
Bruno Gonçalves Pereira <bruno.g.p897@gmail.com>
2023-04-19 16:48:01 -0300

    adicionado SplashScreen.

--- app/src/main/java/dev/brunog/dairyapp/MainActivity.kt
+++ app/src/main/java/dev/brunog/dairyapp/MainActivity.kt
@@ -3,20 +3,16 @@ package dev.brunog.dairyapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dev.brunog.dairyapp.ui.theme.DairyAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DairyAppTheme {
+
             }
         }
     }
