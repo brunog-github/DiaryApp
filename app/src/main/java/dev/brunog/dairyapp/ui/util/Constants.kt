61dcfbb2e7b071cd13c458210360a9b0d08c5f5e
Bruno Gonçalves Pereira <bruno.g.p897@gmail.com>
2023-04-20 16:01:55 -0300

    configurado acesso ao mongodb e google oauth.

---
+++ app/src/main/java/dev/brunog/dairyapp/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package dev.brunog.dairyapp.ui.util
+
+import dev.brunog.dairyapp.BuildConfig
+
+object Constants {
+    const val APP_ID = BuildConfig.APP_ID
+    const val CLIENT_ID = BuildConfig.CLIENT_ID
+}
