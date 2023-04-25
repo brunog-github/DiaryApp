11d1fd0ea93b0e8cf6f2785840c48dd5a75530f5
Bruno Gonçalves Pereira <bruno.g.p897@gmail.com>
2023-04-25 15:12:42 -0300

    Atualizando as dependências.

--- app/src/main/java/dev/brunog/dairyapp/MainActivity.kt
+++ app/src/main/java/dev/brunog/dairyapp/MainActivity.kt
@@ -3,6 +3,32 @@ package dev.brunog.dairyapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
@@ -12,6 +38,7 @@ import dev.brunog.dairyapp.ui.theme.DairyAppTheme
 import dev.brunog.dairyapp.ui.util.Constants.APP_ID
 import io.realm.kotlin.internal.platform.isFrozen
 import io.realm.kotlin.mongodb.App
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
