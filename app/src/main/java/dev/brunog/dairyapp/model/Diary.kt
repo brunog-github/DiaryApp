944471bac8c187ec7bd836c8cb1cadc8662add3d
Bruno Gonçalves Pereira <bruno.g.p897@gmail.com>
2023-04-26 13:04:34 -0300

    Criado o Date Header dos diarios e atualiza as dependências.

--- app/src/main/java/dev/brunog/dairyapp/model/Diary.kt
+++ app/src/main/java/dev/brunog/dairyapp/model/Diary.kt
@@ -1,15 +1,15 @@
 package dev.brunog.dairyapp.model
 
 import io.realm.kotlin.ext.realmListOf
-import io.realm.kotlin.types.ObjectId
 import io.realm.kotlin.types.RealmInstant
 import io.realm.kotlin.types.RealmList
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
 
 class Diary: RealmObject {
     @PrimaryKey
-    var _id: ObjectId = ObjectId.create()
+    var _id: ObjectId = ObjectId.invoke()
     var _ownerId: String = ""
     var title: String = ""
     var description: String = ""
