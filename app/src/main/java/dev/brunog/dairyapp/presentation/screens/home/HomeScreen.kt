d07a3b00bef2ad9a6b34b01c13d0b9becc009831
Bruno Gonçalves Pereira <bruno.g.p897@gmail.com>
2023-04-28 15:12:35 -0300

    Terminado o design da tela inicial.

--- app/src/main/java/dev/brunog/dairyapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/dev/brunog/dairyapp/presentation/screens/home/HomeScreen.kt
@@ -14,14 +14,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.LayoutDirection
 import dev.brunog.dairyapp.model.Diary
 import dev.brunog.dairyapp.presentation.components.EmptyPage
@@ -39,14 +40,16 @@ fun HomeScreen(
     onNavigateToWriteScreen: () -> Unit
 ) {
     var paddingValues by remember { mutableStateOf(PaddingValues()) }
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
 
     HomeNavigationDrawer(
         drawerState = drawerState,
         onSignOutClicked = onSignOutClicked
     ) {
         Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
             topBar = {
-                HomeTopBar(onMenuClicked = onMenuClicked)
+                HomeTopBar(scrollBehavior = scrollBehavior,onMenuClicked = onMenuClicked)
             },
             floatingActionButton = {
                 FloatingActionButton(
